75d6fec5b342cf5aab0c178076c244ca71995e05
Jennie <echenimjennifer@gmail.com>
2022-07-06 07:46:38 +0400

    cleanup unused files

---
+++ pages/_document.jsx
@@ -0,0 +1,57 @@
+import Document, { Html, Main, NextScript } from "next/document";
+import Head from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <meta
+            name="description"
+            content="A community braced for impact in the blockchain space."
+          />
+          <meta
+            name="keywords"
+            content="web3, web3ladies, blockchain ladies, blockchain community ladies, ladiesdotech, blockchain nigeria, web3 nigeria"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/layouts/Footer'
+import Navbar from '../components/layouts/Navbar'
+
+export default function Home() {
+  return (
+    <>
+      <Navbar/>
+
+      <main>
+      </main>
+
+      <Footer/>
+    </>
+  )
+}
