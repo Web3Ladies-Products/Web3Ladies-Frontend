ce6f11a77a557b97a0d4ec8cb93ed8318abcedf4
Jennie <echenimjennifer@gmail.com>
2022-07-06 11:23:37 +0400

    - implement responsive navbar

---
+++ components/icons/ChevronDown.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ChevronDown = ({ width, height, color }) => {
+  return (
+    <svg
+      width={width || "12"}
+      height={height || "8"}
+      viewBox="0 0 12 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.00039 0.799805L6.00039 4.7998L10.0004 0.799805L11.6004 1.5998L6.00039 7.1998L0.400391 1.5998L2.00039 0.799805Z"
+        fill={color || "black"}
+      />
+    </svg>
+  );
+};
+
+export default ChevronDown;
--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,9 +1,150 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "../buttons/Button";
+import ChevronDown from "../icons/ChevronDown";
+import Menu from "../icons/Menu";
+import Close from "../icons/Close";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [showMobileNav, setShowMobileNav] = React.useState(false);
 
-export default Navbar
+  const NAVIGATION_ITEMS = [
+    {
+      name: "About Us",
+      route: "/",
+    },
+    {
+      name: "Mentorship",
+      route: "/",
+    },
+    {
+      name: "Sponsorship",
+      route: "/",
+    },
+    {
+      name: "Hire Talent",
+      route: "/",
+    },
+    {
+      name: "Contact Us",
+      route: "/",
+    },
+    {
+      name: "Blog",
+      route: "/",
+    },
+  ];
+
+  return (
+    <header>
+      <div>
+        <div id="header" className="nav-container nav-lg">
+          <Link className="logo" href="/">
+            <a>
+              <Image
+                src={"/assets/images/web3ladies-logo.svg"}
+                width={"120px"}
+                height={"50px"}
+                alt="web3ladies-logo"
+                className="logo"
+              />
+            </a>
+          </Link>
+
+          <nav className="nav">
+            <ul className="header-list">
+              {NAVIGATION_ITEMS.map((item, index) => {
+                return (
+                  <li key={index}>
+                    <span className="header-list-item">
+                      <Link href={item.route}>
+                        <a href="#">{item.name}</a>
+                      </Link>
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+            <div className="nav-cta">
+              <a
+                href="/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="button outline"
+              >
+                Join Us Today
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className="nav-cta">
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="button outline"
+          >
+            Join Us Today
+          </a>
+        </div>
+
+        <div id="header" className="nav-container nav-sm">
+          <Link href="/">
+            <a>
+              <Image
+                src={"/assets/images/web3ladies-logo.svg"}
+                width={"70px"}
+                height={"40px"}
+                alt="web3ladies-logo"
+                className="logo"
+              />
+            </a>
+          </Link>
+          {!showMobileNav && (
+            <Menu
+              className="menu-icon"
+              handleClick={() => setShowMobileNav(true)}
+            />
+          )}
+
+          {showMobileNav && (
+            <nav className="nav">
+              <Close
+                className="close-icon"
+                handleClick={() => setShowMobileNav(false)}
+              />
+              <div className="hline"></div>
+              <ul>
+                {NAVIGATION_ITEMS.map((item, i) => {
+                  return (
+                    <li key={i} onClick={() => setShowMobileNav(true)}>
+                      <Link href={item.route}>
+                        <a>
+                          {item.name}
+                          <div className="angle active-sign"></div>
+                        </a>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="nav-cta">
+                <a
+                  href="/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="button outline"
+                >
+                  Join Us Today
+                </a>
+              </div>
+            </nav>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
