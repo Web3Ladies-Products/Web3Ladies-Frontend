f5dafb6b4125fd7c4bfb07e18ab779d2042fad7e
Shukurah Bakare <shukurahbakare@outlook.com>
2022-07-24 05:06:40 +0100

    style changes

--- pages/cohorts.jsx
+++ pages/cohorts.jsx
@@ -2,8 +2,26 @@ import React from "react";
 import Image from "next/image";
 import Navbar from "../components/layouts/Navbar";
 import Button from "../components/buttons/Button";
+import Accordion from "../components/accordion/Accordion";
 
 const cohorts = () => {
+  const curriculumItems = [
+    {
+      label: "Cohort <span class='bold'>One</span>",
+      date: "May - August 2020",
+      content: "content",
+    },
+    {
+      label: "Cohort <span class='bold'>Two</span>",
+      date: "May - August 2020",
+      content: "content",
+    },
+    {
+      label: "Cohort <span class='bold'>Three</span>",
+      date: "May - August 2020",
+      content: "content",
+    },
+  ];
   return (
     <>
       <Navbar />
@@ -55,6 +73,17 @@ const cohorts = () => {
           </div>
         </div>
       </main>
+
+      <section className="summary-section" id="curriculum">
+        <div className="container">
+          <Accordion
+            panels={curriculumItems}
+            hasActiveBackground
+            hasFullBorder
+          />
+        </div>
+      </section>
+
       <section className="visit container">
         <div className="container">
           <div className="hero_content">
@@ -75,8 +104,6 @@ const cohorts = () => {
                 src="/assets/images/youtube.png"
                 alt="hero-image"
               />
-
-              {/* <iframe width="253" height="280" src="https://www.youtube.com/embed/24D8MqQTP_w" title="Project #LadiesDoTech" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
             </div>
           </div>
         </div>
@@ -85,7 +112,122 @@ const cohorts = () => {
       <div className="div"></div>
     </>
   );
-  9;
+};
+
+const Label = ({ number }) => {
+  return (
+    <>
+      Cohort<div className="bold">{number}</div>
+    </>
+  );
+};
+
+const Content = ({}) => {
+  return (
+    <>
+      {/* SUMMARY SECTION */}
+      <div className=" summary">
+        <div className="summary-title">
+          <h1>Summary</h1>
+        </div>
+        <div className="summary-content">
+          <div className="summary-text">
+            <p>
+              This is an intensive learning phase of the basics/foundations for
+              all tracks, it serves as the introduction to the track for
+              participants. This phase is for 4weeks, classes will be twice a
+              week (Tuesdays & Thursdays 5 - 7 PM). Across all tracks
+              participants will learn intensively as a beginner, mentors will be
+              on the ground to teach, resources will be shared with the
+              participants, assignments will be given as well, and there will be
+              an eviction at the end of this phase upon final assessment to
+              graduate into the learning phase.
+            </p>
+          </div>
+          <div className="metrics">
+            <h1 className="metrics-heading">Metrics</h1>
+            <ul className="metrics-container">
+              <li>
+                <h3>54</h3>
+                <span className="metrics-text"> Mentees</span>
+              </li>
+              <li>
+                <h3>3</h3>
+                <span className="metrics-text"> Alumni</span>
+              </li>
+              <li>
+                <h3>24</h3>
+                <span className="metrics-text"> Mentors</span>
+              </li>
+            </ul>
+            {/* <div className="numbers">
+                <div>
+                  <h2>54</h2>
+                  <p>Mentees</p>
+                </div>
+                <div>
+                  <h2>85</h2>
+                  <p>Alumni</p>
+                </div>
+                <div>
+                  <h2>24</h2>
+                  <p>Mentors</p>
+                </div>
+              </div> */}
+          </div>
+        </div>
+      </div>
+
+      {/* MENTEES SECTION */}
+      <section className="mentees-wrapper">
+        <div className="">
+          <h1>Mentee Experience</h1>
+          <div className="mentees">
+            <div className="mentee">
+              <Image
+                width={"282px"}
+                height={"308px"}
+                src="/assets/images/mentee.png"
+                alt="mentee-image"
+              />
+              <h1>Janet Simpson</h1>
+              <p>Product Design</p>
+            </div>
+            <div className="mentee">
+              <Image
+                width={"282px"}
+                height={"308px"}
+                src="/assets/images/mentee.png"
+                alt="mentee-image"
+              />
+              <h1>Janet Simpson</h1>
+              <p>Product Design</p>
+            </div>
+            <div className="mentee">
+              <Image
+                width={"282px"}
+                height={"308px"}
+                src="/assets/images/mentee.png"
+                alt="mentee-image"
+              />
+              <h1>Janet Simpson</h1>
+              <p>Product Design</p>
+            </div>
+            <div className="mentee">
+              <Image
+                width={"282px"}
+                height={"308px"}
+                src="/assets/images/mentee.png"
+                alt="mentee-image"
+              />
+              <h1>Janet Simpson</h1>
+              <p>Product Design</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default cohorts;
