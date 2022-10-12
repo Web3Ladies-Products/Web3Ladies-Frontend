22171b02d65fc2b2860fc5999c19ae72ade3c407
Jennie <echenimjennifer@gmail.com>
2022-10-12 20:46:01 +0400

    wip: sponsorship page

---
+++ pages/sponsorship/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../components/layouts/Navbar";
+
+const Sponsorship = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Sponsorship</div>
+    </>
+  );
+};
+
+export default Sponsorship;
