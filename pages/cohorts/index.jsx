e0a5f950f855254110f099178a9f7d7ed5290f40
Jennie <echenimjennifer@gmail.com>
2022-10-12 22:16:12 +0400

    fix build error

--- pages/cohorts/index.jsx
+++ pages/cohorts/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../../components/layouts/Navbar";
 import Footer from "../../components/layouts/Footer";
 import VisitYoutube from "../../components/VisitYoutube";
-import CohortSection from "./cohorts/Cohorts";
+import CohortSection from "../../components/mentorship/cohorts/Cohorts";
 
 const Cohorts = () => {
   return (
