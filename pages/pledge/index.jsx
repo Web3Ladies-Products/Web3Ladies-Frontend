5c6670cbeaae2bdb98c94e9e96e6cc8a1d359de1
Jennie <echenimjennifer@gmail.com>
2022-10-13 08:16:50 +0400

    wip: pledge page

--- pages/pledge/index.jsx
+++ pages/pledge/index.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Footer from "../../components/layouts/Footer";
+import Navbar from "../../components/layouts/Navbar";
 
 const Pledge = () => {
-  return <div>Pledge</div>;
+  return (
+    <>
+      <Navbar />
+      <div className="app-container">
+        <section>
+          <div className="container">
+            <h1 className="section-title center">Take our pledge</h1>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Pledge;
